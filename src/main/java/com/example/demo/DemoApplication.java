048f8d2524c3f711a9af9bdf0a25a9ac4f7660dc
201180082 <1260878527@qq.com>
2022-07-30 05:21:29 +0800

    接口限速

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,11 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import com.google.common.util.concurrent.RateLimiter;
+
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
-
+	private final RateLimiter limiter = RateLimiter.create(100.0);
+	private DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
@@ -17,6 +22,10 @@ public class DemoApplication {
 
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		boolean tryAcquire = limiter.tryAcquire(1, TimeUnit.MILLISECONDS);
+		if (!tryAcquire) {
+			return "429（Too many requests）";
+		}
 		return String.format("Hello %s!", name);
 	}
 //
